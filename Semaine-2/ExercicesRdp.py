84307968830b82adc8f386e3835538d45fb5e66a
PaolaRLuna <116096039+PaolaRLuna@users.noreply.github.com>
2022-10-31 15:39:23 -0400

    Final Update

--- Semaine-2/ExercicesRdp.py
+++ Semaine-2/ExercicesRdp.py
@@ -1,5 +1,4 @@
 # Exercice 1, Partie 1:
-
 # Créer une calculatrice pour fractions incluant les opérations suivantes: 
 
 # Addition (a/b + d/c)
@@ -8,13 +7,13 @@
 # Divisions (a/b / d/c)
 
 # Exercice 1, Partie 2:
-
 # Écrivez vos fonctions de sorte qu'ils ne prennent que deux termes. Ex: fct(fraction1, fraction2).
 
 def calcul(fraction1, fraction2):
     fraction1 = num1/den1
     fraction2 = num2/den2
-    # Exercice 1, Partie 1:
+
+    #Partie 1:
 
     addition = (fraction1 + fraction2)
     soustraction = (fraction1 - fraction2)
@@ -23,8 +22,8 @@ def calcul(fraction1, fraction2):
     
     return addition, soustraction, multiplication, division
 
-fraction1 = 3, 4 # Forme n1/d1, example: 3/4
-fraction2 = 2, 3 # Forme n2/d2, example: 2/3
+fraction1 = 4, 3 # Forme n1/d1, example: 3/4
+fraction2 = 8, 7 # Forme n2/d2, example: 2/3
 
 num1, den1 = fraction1
 num2, den2 = fraction2
@@ -35,12 +34,9 @@ resultat = calcul(fraction1, fraction2)
 # Affichez vos résultats sous forme de fraction et sous forme d'un float.
 
 add, sous, mult, div = resultat
-print(f"Addition: \n- Fraction form: {(num1*den2 + den1*num2)}/{(den1 * den2)} \n- Forme de float: ", add)
-print(f"Soustraction: \n- Fraction form: {(num1*den2 - den1*num2)}/{(den1 * den2)} \n- Forme de float: ", sous)
-print(f"Multiplication: \n- Fraction form: {num1 * num2}/{den1 * den2} \n- Forme de float: ", mult)
-print(f"Division: \n- Fraction form: {num1 * den2}/{den1 * num2} \n- Forme de float: ",div)
+print(f"Addition: \n- Fraction: {(num1*den2 + den1*num2)}/{(den1 * den2)} \n- Float: ", add)
+print(f"Soustraction: \n- Fraction: {(num1*den2 - den1*num2)}/{(den1 * den2)} \n- Float: ", sous)
+print(f"Multiplication: \n- Fraction: {num1 * num2}/{den1 * den2} \n- Float: ", mult)
+print(f"Division: \n- Fraction: {num1 * den2}/{den1 * num2} \n- Float: ",div)
 
 # Testez vos fonctions avec plusieurs fractions pour vous assurer du bon fonctionnement.
-
-#
-# 
