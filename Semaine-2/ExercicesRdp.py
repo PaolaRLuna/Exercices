7a07851517c881ca17266ee1098c77d045a855b2
PaolaRLuna <116096039+PaolaRLuna@users.noreply.github.com>
2022-10-31 15:29:02 -0400

    update variables for fractions

--- Semaine-2/ExercicesRdp.py
+++ Semaine-2/ExercicesRdp.py
@@ -12,8 +12,8 @@
 # Écrivez vos fonctions de sorte qu'ils ne prennent que deux termes. Ex: fct(fraction1, fraction2).
 
 def calcul(fraction1, fraction2):
-    fraction1 = n1/d1
-    fraction2 = n2/d2
+    fraction1 = num1/den1
+    fraction2 = num2/den2
     # Exercice 1, Partie 1:
 
     addition = (fraction1 + fraction2)
@@ -23,23 +23,22 @@ def calcul(fraction1, fraction2):
     
     return addition, soustraction, multiplication, division
 
-fraction1 = 3, 4 # 3/4
-fraction2 = 2, 3 # 2/3
+fraction1 = 3, 4 # Forme n1/d1, example: 3/4
+fraction2 = 2, 3 # Forme n2/d2, example: 2/3
 
-n1, d1 = fraction1
-n2, d2 = fraction2
+num1, den1 = fraction1
+num2, den2 = fraction2
 resultat = calcul(fraction1, fraction2)
-#print(resultat)
 
 # Exercice 1, Partie 3:
 
 # Affichez vos résultats sous forme de fraction et sous forme d'un float.
 
 add, sous, mult, div = resultat
-print(f"Addition: \n- Fraction form: {(n1*d2 + d1*n2)}/{(d1 * d2)} \n- Forme de float: ", add)
-print(f"Soustraction: \n- Fraction form: {(n1*d2 - d1*n2)}/{(d1 * d2)} \n- Forme de float: ", sous)
-print(f"Multiplication: \n- Fraction form: {n1 * n2}/{d1 * d2} \n- Forme de float: ", mult)
-print(f"Division: \n- Fraction form: {n1 * d2}/{d1 * n2} \n- Forme de float: ",div)
+print(f"Addition: \n- Fraction form: {(num1*den2 + den1*num2)}/{(den1 * den2)} \n- Forme de float: ", add)
+print(f"Soustraction: \n- Fraction form: {(num1*den2 - den1*num2)}/{(den1 * den2)} \n- Forme de float: ", sous)
+print(f"Multiplication: \n- Fraction form: {num1 * num2}/{den1 * den2} \n- Forme de float: ", mult)
+print(f"Division: \n- Fraction form: {num1 * den2}/{den1 * num2} \n- Forme de float: ",div)
 
 # Testez vos fonctions avec plusieurs fractions pour vous assurer du bon fonctionnement.
 
