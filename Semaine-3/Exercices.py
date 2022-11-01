df0c38ce7e74ece305470ac2af8144e3b17c911b
PaolaRLuna <116096039+PaolaRLuna@users.noreply.github.com>
2022-10-31 21:31:25 -0400

    uptade exercice5

--- Semaine-3/Exercices.py
+++ Semaine-3/Exercices.py
@@ -11,40 +11,40 @@
 
 # seasons = spring, summer, fall, winter
 
-def saison_naissance():
-    jour_fete = int(input("Entrer votre jour de fête : "))
-    mois_fete = int(input("Entrer votre mois de fête en nombre (ex. oct = 10) : "))
+# def saison_naissance():
+#     jour_fete = int(input("Entrer votre jour de fête : "))
+#     mois_fete = int(input("Entrer votre mois de fête en nombre (ex. oct = 10) : "))
     
-    if mois_fete in (1, 2):
-        return "Votre saison est l'hiver"
-    elif mois_fete in (4, 5):
-        return "Votre saison est le printemps"
-    elif mois_fete in (7, 8):
-        return "Votre saison est l'ete"
-    elif mois_fete in (10, 11):
-        return "Votre saison est l'automne"
-    else:
-        if jour_fete <= 21 and mois_fete == 3:
-            return "Votre saison est l'hiver"
-        elif jour_fete > 21 and mois_fete == 3:
-            return "Votre saison est le printemps"
-        elif jour_fete <= 21 and mois_fete == 6:
-            return "Votre saison est le printemps"
-        elif jour_fete > 21 and mois_fete == 6:
-            return "Votre saison est l'ete"
-        elif jour_fete <= 21 and mois_fete == 9:
-            return "Votre saison est l'ete"
-        elif jour_fete > 21 and mois_fete == 9:
-            return "Votre saison est l'automne"
-        elif jour_fete <= 21 and mois_fete == 12:
-            return "Votre saison est l'automne"
-        elif jour_fete > 21 and mois_fete == 12:
-            return "Votre saison est l'hiver"
-        else:
-            return "Ce n'est pas une date valide"
+#     if mois_fete in (1, 2):
+#         return "Votre saison est l'hiver"
+#     elif mois_fete in (4, 5):
+#         return "Votre saison est le printemps"
+#     elif mois_fete in (7, 8):
+#         return "Votre saison est l'ete"
+#     elif mois_fete in (10, 11):
+#         return "Votre saison est l'automne"
+#     else:
+#         if jour_fete <= 21 and mois_fete == 3:
+#             return "Votre saison est l'hiver"
+#         elif jour_fete > 21 and mois_fete == 3:
+#             return "Votre saison est le printemps"
+#         elif jour_fete <= 21 and mois_fete == 6:
+#             return "Votre saison est le printemps"
+#         elif jour_fete > 21 and mois_fete == 6:
+#             return "Votre saison est l'ete"
+#         elif jour_fete <= 21 and mois_fete == 9:
+#             return "Votre saison est l'ete"
+#         elif jour_fete > 21 and mois_fete == 9:
+#             return "Votre saison est l'automne"
+#         elif jour_fete <= 21 and mois_fete == 12:
+#             return "Votre saison est l'automne"
+#         elif jour_fete > 21 and mois_fete == 12:
+#             return "Votre saison est l'hiver"
+#         else:
+#             return "Ce n'est pas une date valide"
 
 
-print(saison_naissance())
+# print(saison_naissance())
 
 
     
@@ -55,6 +55,24 @@ print(saison_naissance())
 # permettant de vérifier que les nombres ne sont pas les deux pairs ou impairs, et affichez la 
 # phrase suivante: Votre nombre impair est le x, votre nombre pair est le y et le résultat de 
 # leur division est égal à z. Vous ne devez qu'afficher 3 chiffres après le point.
+
+from re import X
+
+
+def type_nombre():
+    x = int(input("Entrer un nombre pair ou impair : "))
+    y = int(input("Entrer un nombre pair ou impair : "))
+
+    modulo = x % y
+    z = y / x
+
+    if modulo != 0:
+        return f"Votre nombre impair es le {X}, votre nombre pair est le {y} \
+        et le resultat de leur division est egal a {z:.3f})"
+    else:
+        return f"Vos nombres sont pairs"
+
+print(type_nombre())
 
 # Exercice 6:
 
