45427f1cf52f3f2b5e22aa53a5414272dcc014ec
PaolaRLuna <116096039+PaolaRLuna@users.noreply.github.com>
2022-11-10 15:52:58 -0500

    Update on code

--- Semaine 4/Exercice6.py
+++ Semaine 4/Exercice6.py
@@ -1,2 +1,13 @@
 # Écrire un programme demandant un entier à un utilisateur 
 # et lui retournant sa parité. Ex.: Le nombre «4» est paire.
+
+def parite():
+
+    nombre = int(input("Entrer un entier: "))
+
+    if nombre % 2 == 0:
+        print(f"Le nombre {nombre} est pair")
+    else:
+        print(f"Le nombre {nombre} est impair")
+
+parite()
