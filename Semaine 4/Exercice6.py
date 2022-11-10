a47f9d04eca6f62cda2db40e77d7b7d53b8aa38c
PaolaRLuna <116096039+PaolaRLuna@users.noreply.github.com>
2022-11-10 10:56:39 -0500

    update

---
+++ Semaine 4/Exercice6.py
@@ -0,0 +1,2 @@
+# Écrire un programme demandant un entier à un utilisateur 
+# et lui retournant sa parité. Ex.: Le nombre «4» est paire.
---
+++ Semaine 4/Exercice7.py
@@ -0,0 +1,5 @@
+# Écrire un programme demandant à l'utilisateur de rentrer un 
+# nombre entre 2 et 20, vérifier si ce dernier est bel et bien 
+# entre 2 et 20, et indiquez-lui si son nombre est un nombre 
+# premier (ayant aucun autre facteur entier que 1 et lui-même)
+
