39cd9925b3a102f6d7f8d955797bc970c101a7c8
PaolaRLuna <116096039+PaolaRLuna@users.noreply.github.com>
2022-11-10 19:33:51 -0500

    update on code

--- Semaine-3/Exercice-8.py
+++ Semaine-3/Exercice-8.py
@@ -7,45 +7,45 @@
 # après la virgule.
 
 
-def calcul(fraction1, fraction2):
-    fraction1 = num1/den1
-    fraction2 = num2/den2
+# def calcul(fraction1, fraction2):
+#     fraction1 = num1/den1
+#     fraction2 = num2/den2
 
-    addition = (fraction1 + fraction2)
-    soustraction = (fraction1 - fraction2)
-    multiplication = (fraction1 * fraction2)
-    division = (fraction1 / fraction2)
+#     addition = (fraction1 + fraction2)
+#     soustraction = (fraction1 - fraction2)
+#     multiplication = (fraction1 * fraction2)
+#     division = (fraction1 / fraction2)
 
-    return addition, soustraction, multiplication, division
-
-
-def user_input():
-    def input_fraction():
-        num = int(input("Num: "))
-        den = int(input("Denom: "))
-        return num, den
-
-    print("Entrer nombres fraction 1: ")
-    fraction1 = input_fraction()
-    print("Entrer nombres fraction 2: ")
-    fraction2 = input_fraction()
-    return fraction1, fraction2
+#     return addition, soustraction, multiplication, division
 
 
-fraction1, fraction2 = user_input()
-num1, den1 = fraction1
-num2, den2 = fraction2
+# def user_input():
+#     def input_fraction():
+#         num = int(input("Num: "))
+#         den = int(input("Denom: "))
+#         return num, den
 
-resultat = calcul(fraction1, fraction2)
+#     print("Entrer nombres fraction 1: ")
+#     fraction1 = input_fraction()
+#     print("Entrer nombres fraction 2: ")
+#     fraction2 = input_fraction()
+#     return fraction1, fraction2
 
-add, sous, mult, div = resultat
-print(
-    f"Addition: \n- Fraction: {(num1*den2 + den1*num2)}/{(den1 * den2)} \n- Float:  {add:.3f}")
-print(
-    f"Soustraction: \n- Fraction: {(num1*den2 - den1*num2)}/{(den1 * den2)} \n- Float: {sous:.3f}")
-print(
-    f"Multiplication: \n- Fraction: {num1 * num2}/{den1 * den2} \n- Float: {mult:.3f}")
-print(f"Division: \n- Fraction: {num1 * den2}/{den1 * num2} \n- Float: {div:.3f}")
+
+# fraction1, fraction2 = user_input()
+# num1, den1 = fraction1
+# num2, den2 = fraction2
+
+# resultat = calcul(fraction1, fraction2)
+
+# add, sous, mult, div = resultat
+# print(
+#     f"Addition: \n- Fraction: {(num1*den2 + den1*num2)}/{(den1 * den2)} \n- Float:  {add:.3f}")
+# print(
+#     f"Soustraction: \n- Fraction: {(num1*den2 - den1*num2)}/{(den1 * den2)} \n- Float: {sous:.3f}")
+# print(
+#     f"Multiplication: \n- Fraction: {num1 * num2}/{den1 * den2} \n- Float: {mult:.3f}")
+# print(f"Division: \n- Fraction: {num1 * den2}/{den1 * num2} \n- Float: {div:.3f}")
 
 
 # Partie 2:
@@ -55,71 +55,67 @@ print(f"Division: \n- Fraction: {num1 * den2}/{den1 * num2} \n- Float: {div:.3f}
 #Dentro de match en la linea de la indentacion de case se puede imprimir directamente, print(f' etc),
 # Definir cada operacion y desoupes llamar a cada operacion y es importante de habilitar la operacion para un float
 
-def calcul():
-    fraction1 = num1/den1
-    fraction2 = num2/den2
+def calcul(num1, den1, num2, den2):
+    
+    def addition():
+        num = num1*den2 + den1*num2
+        denum = den1 * den2
+        return num/denum
+    
+    def soustraction():
+        num = num1*den2 - den1*num2
+        denum = den1 * den2
+        return num/denum
 
+    def produit():
+        num = num1 * num2
+        denum = den1 * den2
+        return num/denum
 
-    def input_operation(operation, fraction1, fraction2):
-        #""
-        resultat = ""
-        match(operation):
-            case("+"):
-                resultat = addition(fraction1, fraction2)
-                return f'Resultat: {resultat}'
-            case("-"):
-                resultat = soustraction(fraction1,fraction2)
-                return resultat
-            case ("*"):
-                resultat = produit(fraction1,fraction2)
-                return resultat
-            case("/"):
-                resultat = division(fraction1,fraction2)
-                return resultat
+    def division():
+        num = num1 * den2
+        denum = den1 * num2
+        return num/denum
 
-        if operation_type() == addition:
-            return fraction1 + fraction2
+    return addition, soustraction, produit, division
 
-        elif operation == soustraction:
-            return fraction1 - fraction2
+def input_operation(operation, a, b, c, d):
+    #""
+    match(operation, a, b, c, d):
+        case("1"):
+            resultat = a
+            return resultat
+        case("2"):
+            resultat = b
+            return resultat
+        case ("3"):
+            resultat = c
+            return resultat
+        case("4"):
+            resultat = d
+            return resultat
+    return print(f'The result is {resultat}')
 
-        elif operation == produit:
-            return fraction1 * fraction2
-
-        elif operation == division:
-            return fraction1 / fraction2
-    print(resultat)
-    return 
 
 
 def user_input():
-    def operation_type():
-        def input_fraction():
-            num = int(input("Num: "))
-            den = int(input("Denom: "))
-            return num, den
+    def input_fraction():
+        num = int(input("Num: "))
+        den = int(input("Denom: "))
+        return num, den
 
-        print("Entrer type d'operation: ")
-        operation = input_fraction()
-        
-        return operation
+    print("1.Addition \n2.Soustraction \n3.Produit \n4.Division")
+    operation = input("Entrer type d'operation: ")
     print("Entrer nombres fraction 1: ")
     fraction1 = input_fraction()
     print("Entrer nombres fraction 2: ")
     fraction2 = input_fraction()
-    return fraction1, fraction2
+    return fraction1, fraction2, operation
 
-fraction1, fraction2 = user_input()
+fraction1, fraction2, operation = user_input()
 num1, den1 = fraction1
 num2, den2 = fraction2
+a, b, c, d = calcul(num1, den1, num2, den2)
 
-resultat = calcul(fraction1, fraction2)
 
-add, sous, mult, div = resultat
-print(
-    f"Addition: \n- Fraction: {(num1*den2 + den1*num2)}/{(den1 * den2)} \n- Float: ", add)
-print(
-    f"Soustraction: \n- Fraction: {(num1*den2 - den1*num2)}/{(den1 * den2)} \n- Float: ", sous)
-print(
-    f"Multiplication: \n- Fraction: {num1 * num2}/{den1 * den2} \n- Float: ", mult)
-print(f"Division: \n- Fraction: {num1 * den2}/{den1 * num2} \n- Float: ", div)
+
