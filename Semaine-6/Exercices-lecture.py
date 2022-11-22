4b00fe40dbe0f2a4af9424a542d5f381d2a5bb6b
PaolaRLuna <116096039+PaolaRLuna@users.noreply.github.com>
2022-11-22 09:38:14 -0500

    changes

--- Semaine-6/Exercices-lecture.py
+++ Semaine-6/Exercices-lecture.py
@@ -4,10 +4,14 @@ menu = { 1 : "Keven Presseau-St-Laurent - Concepts de programmation 1",
             4 : "Anna Claudia Onofri - Anglais de niveau collégial"
             }
 
-with open("Semaine-6/bdd", "w") as file:
+[v for k, v in menu.items() if "Keven" in k.lower()]
 
-        delimiter = ""
-        for value in menu.values():
-            cle = delimiter.join(i for i in value) + "\n"
-            file.write(cle)
+# Split method
+#https://stackoverflow.com/questions/10484261/find-dictionary-items-whose-key-matches-a-substring
+# with open("Semaine-6/bdd", "w") as file:
+
+#         delimiter = ""
+#         for value in menu.values():
+#             cle = delimiter.join(i for i in value) + "\n"
+#             file.write(cle)
         
