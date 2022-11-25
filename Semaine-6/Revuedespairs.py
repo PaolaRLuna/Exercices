e25acb61ef33531680710e0e28f56dc879670a4e
PaolaRLuna <116096039+PaolaRLuna@users.noreply.github.com>
2022-11-25 10:59:33 -0500

    code changes for part 3

--- Semaine-6/Revuedespairs.py
+++ Semaine-6/Revuedespairs.py
@@ -9,60 +9,90 @@
 # sélection, afficher le nom de l'enseignant et le nom du cours à l'écran.
 
 def menu_dict():
-    print("Cours:")
-    menu = { 1 : "Keven Presseau-St-Laurent - Concepts de programmation 1",
-            2 : "Nicolas Thavonekham Robidoux - Logique mathématique pour l'informatique",
-            3 : "Keven Presseau-St-Laurent - Systèmes d'exploitation",
-            4 : "Anna Claudia Onofri - Anglais de niveau collégial"
+
+    print("\nMENU")
+    cours = { 1 : "Concepts de programmation 1",
+            2 : "Logique mathématique",
+            3 : "Systèmes d'exploitation",
+            4 : "Anglais de niveau collégial"
+            }
+
+    for key, value in cours.items():
+        print(f"{key}.{value}")
+
+    prof = { 1 : "Keven Presseau-St-Laurent",
+             2: "Nicolas Thavonekham Robidoux" ,
+             3: "Keven Presseau-St-Laurent",
+             4 : "Anna Claudia Onofri" 
             }
 
     condition = False
     while not condition:
 
-        user_input = int(input("Choisir un nombre entre 1 et 4, si vous chercher un enseignant la lettre 'C': "))
-        if user_input in menu:
+        user_input = input("Choisir un numéro (Ex. 1) pour afficher l'enseignant: ")
+        if user_input == "1":
+            #condition = True
+            print(f"{prof.get(1)}")
+        elif user_input == "2":
+            #condition = True
+            print(f"{prof.get(2)}")
+        elif user_input == "3":
+            #condition = True
+            print(f"{prof.get(3)}")
+        elif user_input == "4":
+            #condition = True
+            print(f"{prof.get(4)}")
+        else:
             condition = True
-            option = (menu[user_input])
-        elif user_input == "C":
-            condition = True
-            search(menu)
-        
-    return option, menu
+
+    return cours, prof
 
 # Partie 2:
-# En se basant sur l'exercice 3, créer un fichier bdd.txt fonctionnant comme une base de données et remplir le dictionnaire à 
+# En se basant sur la partie 1, créer un fichier bdd.txt fonctionnant comme une base de données et remplir le dictionnaire à 
 # partir de ce fichier. Pour vous faciliter la tâche, vous pouvez écrire les informations de la manière suivante:
 # Nom de cours 1
 # Nom de prof 1
 # Nom de cours 2
+#ds1: dict[int, str]
 
-def fichier(donnees: dict[int, str]):
+def fichier():
 
-    with open("bdd", "w") as file:
+    cours, prof = menu_dict()
 
-        delimiter = ""
-        for value in donnees.values():
-            cle = delimiter.join(i for i in value) + "\n"
-            file.write(cle)
+    infos = {}
+    for c, p in list(zip(cours.values(), prof.values())):
+        infos [c] = p
 
+    with open("bdd.txt", "w", encoding='utf8') as fichier:
 
-def search(donnees: dict[int, str]):
+        #https://stackoverflow.com/questions/34069702/write-dict-to-file-with-each-key-value-pair-on-a-separate-line
+        #Auteur: JCVanHamme
+        #Idée de comment imprimer dans un fichier à partir du dictionnaire
+        for matiere, prof in infos.items():
+            print(f"{matiere} \n{prof}", file=fichier)
+
+    return infos
+
+def search():
+
+    donnees = fichier()
 
     search_user = input("Entrer nombre d'enseignant: ")
-    for value in donnees.values():
-        if search_user in donnees:
+    for value in donnees:
+        if search_user in donnees.values():
             print(f"L'enseignant {value} est dans la liste")
 
+search()
 # Partie 3:
 
 #En se basant sur la partie 2, modifier le menu utilisateur en y ajoutant une option lui permettant de faire une recherche d'enseignant. 
 # Vérifier si l'enseignant entré par l'utilisateur est présent dans votre liste de cours et indiquer le résultat à la console.        
 
-def output():
+# def output():
 
-    option, menu = menu_dict()
-    print(f"Cours: {option}")
-    fichier(menu)
+#     option, menu = menu_dict()
+#     print(f"Cours: {option}")
+#     fichier(menu)
 
-if __name__ == '__main__':
-    output()
+# if __name__ == '__main__':
+#     output()
