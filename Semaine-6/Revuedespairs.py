db44ad3986f4a9be29ff7377b84f053714d1d7fb
PaolaRLuna <116096039+PaolaRLuna@users.noreply.github.com>
2022-11-26 22:32:15 -0500

    Update Revuedespairs.py

--- Semaine-6/Revuedespairs.py
+++ Semaine-6/Revuedespairs.py
@@ -8,17 +8,18 @@
 #  à l'utilisateur d'en sélectionner 1. Lorsque l'utilisateur à fait sa 
 # sélection, afficher le nom de l'enseignant et le nom du cours à l'écran.
 
-def menu_dict():
+def data():
 
-    print("\nMENU")
+    choisir = { 1 : "Choisir un cours",
+            2 : "Chercher un enseignant",
+            3 : "Ajouter un cours et un prof"
+            }
+    
     cours = { 1 : "Concepts de programmation 1",
             2 : "Logique mathématique",
             3 : "Systèmes d'exploitation",
             4 : "Anglais de niveau collégial"
             }
-
-    for key, value in cours.items():
-        print(f"{key}.{value}")
 
     prof = { 1 : "Keven Presseau-St-Laurent",
              2: "Nicolas Thavonekham Robidoux" ,
@@ -26,27 +27,52 @@ def menu_dict():
              4 : "Anna Claudia Onofri" 
             }
 
+    return choisir, cours, prof
+
+def menu():
+
+    choisir, _, _ = data()
+    
     condition = False
     while not condition:
 
-        user_input = input("Choisir un numéro (Ex. 1) pour afficher l'enseignant: ")
+        print("\nMENU")
+        for key, value in choisir.items():
+            print(f"{key}.{value}")
+
+        user_input = input("Choisir un numéro (Ex. 1): ")
         if user_input == "1":
-            #condition = True
-            print(f"{prof.get(1)}")
+            submenu_choisir()
         elif user_input == "2":
-            #condition = True
-            print(f"{prof.get(2)}")
+            search(fichier())
         elif user_input == "3":
-            #condition = True
-            print(f"{prof.get(3)}")
-        elif user_input == "4":
-            #condition = True
-            print(f"{prof.get(4)}")
+            input_user()
         else:
             condition = True
 
-    return cours, prof
+def submenu_choisir():
 
+    _, cours, prof = data()
+        
+    print("\nSUB MENU")
+    for key, value in cours.items():
+        print(f"{key}.{value}")
+
+    condition = False
+    while not condition:
+        user_input = input("Choisir un cours (Ex. 1): ")
+        if user_input == "1":
+            condition = True
+            print(f"{cours.get(1)} - {prof.get(1)}")
+        elif user_input == "2":
+            condition = True
+            print(f"{cours.get(2)} - {prof.get(2)}")
+        elif user_input == "3":
+            condition = True
+            print(f"{cours.get(3)} - {prof.get(3)}")
+        elif user_input == "4":
+            condition = True
+            print(f"{cours.get(4)} - {prof.get(4)}")
 # Partie 2:
 # En se basant sur la partie 1, créer un fichier bdd.txt fonctionnant comme une base de données et remplir le dictionnaire à 
 # partir de ce fichier. Pour vous faciliter la tâche, vous pouvez écrire les informations de la manière suivante:
@@ -57,10 +83,12 @@ def menu_dict():
 
 def fichier():
 
-    cours, prof = menu_dict()
+    _, cours, prof = data()
+    courses = cours.values()
+    profs = prof.values()
 
     infos = {}
-    for c, p in list(zip(cours.values(), prof.values())):
+    for c, p in list(zip(courses, profs)):
         infos [c] = p
 
     with open("bdd.txt", "w", encoding='utf8') as fichier:
@@ -71,28 +99,55 @@ def fichier():
         for matiere, prof in infos.items():
             print(f"{matiere} \n{prof}", file=fichier)
 
+
     return infos
 
-def search():
+def read_file():
+    pass
 
-    donnees = fichier()
-
-    search_user = input("Entrer nombre d'enseignant: ")
-    for value in donnees:
-        if search_user in donnees.values():
-            print(f"L'enseignant {value} est dans la liste")
-
-search()
 # Partie 3:
 
 #En se basant sur la partie 2, modifier le menu utilisateur en y ajoutant une option lui permettant de faire une recherche d'enseignant. 
-# Vérifier si l'enseignant entré par l'utilisateur est présent dans votre liste de cours et indiquer le résultat à la console.        
+# Vérifier si l'enseignant entré par l'utilisateur est présent dans votre liste de cours et indiquer le résultat à la console.     
 
-# def output():
+def search(donnees:dict[str,str]):
 
-#     option, menu = menu_dict()
-#     print(f"Cours: {option}")
-#     fichier(menu)
+    condition = False
+    while not condition:
+        search_user = input("Entrer nombre d'enseignant: ")
+
+        if search_user == '':
+            condition = True
+        #https://stackoverflow.com/questions/18763905/print-out-message-only-once-from-the-for-loop
+        #Auteur: HennyH
+        #Idee de comment retourner une seule fois un print
+        elif any(search_user in value for value in donnees.values()):
+            print(f"L'enseignant {search_user} est dans la liste")
+            condition = True
+        else:
+            print("Not found")
+
+
+menu()
+
+# Partie 4:
+
+# Offrir à l'utilisateur une nouvelle option au menu lui permettant d'ajouter un cours et un nom d'enseignant à la base de données de la partie 2. 
+# Une fois les données utilisateurs entrées, ajouter les informations à la fin du document bdd.txt
+
+def input_user():
+
+    _, cours, prof = data()
+
+    with open("bdd.txt","r", encoding='utf8')
+    infos = fichier()
+    condition = False
+    while not condition:
+        ajout_cours = input("Ajouter un nom de cours: ")
+        ajout_enseignant = input("Ajouter un enseignant")
+        infos[ajout_cours] = ajout_enseignant
+
+        with open("bdd.txt", "a", encoding='utf8') as fichier:
 
 # if __name__ == '__main__':
 #     output()
