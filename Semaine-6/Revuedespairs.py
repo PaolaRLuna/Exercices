9865036607be29ee55ade7bb50525319e6119e53
PaolaRLuna <116096039+PaolaRLuna@users.noreply.github.com>
2022-12-03 23:01:45 -0500

    update on code

--- Semaine-6/Revuedespairs.py
+++ Semaine-6/Revuedespairs.py
@@ -24,15 +24,20 @@ def data():
 
     return cours, prof
 
-def fichier(cours : dict, prof : dict):
 
-    _, cours, prof = data()
+def course_prof_dic_tup(cours:dict, prof: dict):
     courses = cours.values()
     prof = prof.values()
-
     infos = {}
     for c, p in list(zip(courses, prof)):
         infos [c] = p
+    return infos
+
+
+def fichier():
+
+    cours, prof = data()
+    infos = course_prof_dic_tup(cours,prof)
 
     with open("bdd.txt", "w", encoding='utf8') as fichier:
 
@@ -42,61 +47,64 @@ def fichier(cours : dict, prof : dict):
         for matiere, prof in infos.items():
             print(f"{matiere} \n{prof}", file=fichier)
 
-    return infos
 
-def read_file(path : str):
+
+def read_file():
     
-    with open(path, "r", encoding='utf8') as fichier:
+    with open("bdd.txt", "r", encoding='utf8') as fichier:
+        lignes = fichier.readlines()
+        d_cours = {}
+        d_profs = {}
 
-        d_cours = []
-        d_profs = []
-        donees = fichier.readlines
-        
-        for i, ligne in enumerate(donees):
-            if i % 2 == 1:
-                cours = ligne.strip()
-            elif i % 2 == 0:
-                prof = ligne.strip()
-            d_cours.append(cours)
-            d_profs.append(prof)
+        numc = 0
+        nump = 0        
+        for i, ligne in enumerate(lignes):
+
+            if i == 0 or i % 2 == 0:
+                cours = ligne.strip("\n")
+                numc += 1
+                d_cours[numc] = cours
+            else:
+                prof = ligne.strip("\n")
+                nump += 1
+                d_profs[nump] = prof
+
     return d_cours, d_profs
 
+
 def menu():
+    fichier()
+    cours, prof = read_file()
 
     choisir = { 1 : "Choisir un cours",
             2 : "Chercher un enseignant",
-            3 : "Ajouter un cours et un prof"
+            3 : "Ajouter un cours et un prof",
+            4 : "Sortir"
             }
 
-    condition = False
-    while not condition:
+    sortie = False
+    while not sortie:
 
         print("\nMENU")
         for key, value in choisir.items():
             print(f"{key}.{value}")
 
-        user_input = input("Choisir un numéro (Ex. 1): ")
-        if user_input == "1":
-            submenu_choisir()
-        elif user_input == "2":
-            search(fichier())
-        elif user_input == "3":
-            input_user()
-        else:
-            condition = True
+        user_input = int(input("Choisir un numéro (Ex. 1): "))
+        match user_input:
+            case 1:
+                submenu_choisir(cours, prof)
+            case 2:
+                search(course_prof_dic_tup(cours, prof))
+            case 3:
+                pass#input_user()
+            case 4:
+                sortie = True
+            case _:
+                print("Choix invalide")
 
-def creation_choix():
-
-    cours, profs = read_file("bdd.txt")
-
-    cours = { k+1 : v for k, v in cours }
-    profs = { k+1 : v for k, v in profs }
-    return cours, profs
 
 def submenu_choisir(cours : dict, prof : dict):
 
-    cours, prof = creation_choix()
-        
     print("\nSUB MENU")
     for key, value in cours.items():
         print(f"{key}.{value}")
@@ -105,17 +113,18 @@ def submenu_choisir(cours : dict, prof : dict):
     while not condition:
         user_input = input("Choisir un cours (Ex. 1): ")
         if user_input == "1":
-            condition = True
             print(f"{cours.get(1)} - {prof.get(1)}")
         elif user_input == "2":
-            condition = True
             print(f"{cours.get(2)} - {prof.get(2)}")
         elif user_input == "3":
-            condition = True
             print(f"{cours.get(3)} - {prof.get(3)}")
         elif user_input == "4":
             condition = True
             print(f"{cours.get(4)} - {prof.get(4)}")
+        else:
+            print("Choix invalide")
+
+
 # Partie 2:
 # En se basant sur la partie 1, créer un fichier bdd.txt fonctionnant comme une base de données et remplir le dictionnaire à 
 # partir de ce fichier. Pour vous faciliter la tâche, vous pouvez écrire les informations de la manière suivante:
@@ -129,7 +138,7 @@ def submenu_choisir(cours : dict, prof : dict):
 #En se basant sur la partie 2, modifier le menu utilisateur en y ajoutant une option lui permettant de faire une recherche d'enseignant. 
 # Vérifier si l'enseignant entré par l'utilisateur est présent dans votre liste de cours et indiquer le résultat à la console.     
 
-def search(donnees:dict[str,str]):
+def search(profs:dict[str,str]):
 
     condition = False
     while not condition:
@@ -137,16 +146,16 @@ def search(donnees:dict[str,str]):
 
         if search_user == '':
             condition = True
-        #https://stackoverflow.com/questions/18763905/print-out-message-only-once-from-the-for-loop
-        #Auteur: HennyH
-        #Idee de comment retourner une seule fois un print
-        elif any(search_user in value for value in donnees.values()):
+#         #https://stackoverflow.com/questions/18763905/print-out-message-only-once-from-the-for-loop
+#         #Auteur: HennyH
+#         #Idee de comment retourner une seule fois un print
+        elif any(search_user in value for value in profs.values()):
             print(f"L'enseignant {search_user} est dans la liste")
             condition = True
         else:
             print("Not found")
 
-
+menu()
 
 
 # Partie 4:
@@ -154,17 +163,17 @@ def search(donnees:dict[str,str]):
 # Offrir à l'utilisateur une nouvelle option au menu lui permettant d'ajouter un cours et un nom d'enseignant à la base de données de la partie 2. 
 # Une fois les données utilisateurs entrées, ajouter les informations à la fin du document bdd.txt
 
-def input_user():
+# def input_user():
 
-    with open("bdd.txt","a", encoding='utf8') as fichier:
-        condition = False
-        while not condition:
-            ajout_cours = input("Ajouter un nom de cours: ")
-            fichier.write(f"{ajout_cours}\n")
-            ajout_enseignant = input("Ajouter un enseignant")
-            fichier.write(f"{ajout_enseignant}\n")
-            condition = True
+#     with open("bdd.txt","a", encoding='utf8') as fichier:
+#         condition = False
+#         while not condition:
+#             ajout_cours = input("Ajouter un nom de cours: ")
+#             fichier.write(f"{ajout_cours}\n")
+#             ajout_enseignant = input("Ajouter un enseignant")
+#             fichier.write(f"{ajout_enseignant}\n")
+#             condition = True
 
-menu()
+# menu()
 # if __name__ == '__main__':
 #     output()
